0cac9591b92dbc8b4b0130be39d75e4058b74b4f
Moody Rahman <moody.byte@gmail.com>
2023-09-07 19:16:51 -0400

    copy emulator wasm code to frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,17 @@
+
+<script>
+    import { add } from "$lib/wasm/release.js";
+    $: count = 0
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<p>
+    lets run some wasm code!
+</p>
+
+<p>
+    {add(4, 5)}
+</p>
