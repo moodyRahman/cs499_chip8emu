53290ef89552b53a2965ca586aa6f85b1e8a3833
Moody Rahman <moody.byte@gmail.com>
2023-09-10 19:15:35 -0400

    emu skeleton

--- emulator/assembly/cpu.ts
+++ emulator/assembly/cpu.ts
@@ -1,13 +1,25 @@
 var arr = new Array<i32>(10);
+class CPU {
+  ram: Array<i32> = new Array<i32>(1024);
+  rom: Array<string> = new Array<string>(1024);
+  constructor() {}
+}
+
+const cpu = new CPU();
 
 export function write_to_memory(location: i32, value: i32): void {
-  arr[location] = value;
+  cpu.ram[location] = value;
 }
 
 export function read_from_memory(location: i32): i32 {
-  return arr[location];
+  return cpu.ram[location];
 }
 
 export function read_all_memory(): Array<i32> {
   return arr;
+}
+
+export function load_rom(contents: string): void {
+  cpu.rom = [];
+  cpu.rom = contents.split("\n");
 }
