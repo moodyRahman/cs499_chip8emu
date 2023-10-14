3a9db57dfed6d19dec599fcccac5fd26cd23a6ca
Moody Rahman <moody.byte@gmail.com>
2023-10-14 13:37:01 -0400

    render display

---
+++ frontend/src/components/Octet.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Pixel from "./Pixel.svelte";
+
+    export let data: number;
+    $: data = 0;
+</script>
+
+<span>
+    {#each data.toString(2).padStart(8, "0") as x }
+    
+        <Pixel pixel={parseInt(x)} />
+
+    {/each}
+</span>
+
+
+<style>
+    span {
+        padding: 0px;
+        margin: 0px;
+    }
+</style>
---
+++ frontend/src/components/Pixel.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let pixel: number;
+    $: pixel = 0
+</script>
+
+
+<span
+        style:background-color={pixel === 0?"white":"black"}
+>
+<pre> </pre>
+</span>
+
+
+<style>
+    pre {
+        display: inline;
+        width: 10px;
+    }
+
+    span {
+        width: 10px;
+    }
+</style>
