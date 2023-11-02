e6f6281e475613e03f2b8c3719737172b66b78fe
Moody Rahman <moody.byte@gmail.com>
2023-11-02 16:28:01 -0400

    enable backend server cors

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,19 @@
 
 import express from "express";
 import { router } from "./routes/index.js";
+import cors from "cors";
 
 const app = express()
 const port = 3000
+app.use(cors())
+
+// http://localhost:3000/assets/roms/a.png
+app.use("/assets", express.static("./assets"))
 
 app.get('/', (req, res) => {
     res.send('backend online')
 })
 
-// http://localhost:3000/assets/roms/a.png
-app.use("/assets", express.static("./assets"))
 
 app.use(router)
 
