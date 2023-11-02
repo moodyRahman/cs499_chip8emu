7127eefb4b7810b2c688aeb981ab1fb2c4843f8e
Moody Rahman <moody.byte@gmail.com>
2023-11-02 11:17:05 -0400

    static file serve

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 
 import express from "express";
+import { router } from "./routes/index.js";
 
 const app = express()
 const port = 3000
@@ -7,6 +8,12 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('backend online')
 })
+
+// http://localhost:3000/assets/roms/a.png
+app.use("/assets", express.static("./assets"))
+
+app.use(router)
+
 
 app.listen(port, () => {
     console.log(`app online at http://localhost:3000`)
