9adce1c148dbc92c6038eef377856585e06d26d6
Moody Rahman <moody.byte@gmail.com>
2023-11-05 18:17:15 -0500

    scrap paginated disassembler

--- frontend/src/components/Disassembler.svelte
+++ frontend/src/components/Disassembler.svelte
@@ -4,24 +4,34 @@
 
     export let raw_rom: Uint8Array = new Uint8Array();
     export let rom_name: string;
+    
+    let rom: Uint16Array;  // internal copy of the ROM
+    let page = 0;  // current viewing page
+    let display: Uint16Array;  // slice of rom of length height
+    
+    let pc: number = 512;
 
-    let rom: Uint16Array;
-    let page = 0;
-    let display: Uint16Array;
-
+    // memory of arbitrary ram address to read
     let arbit = 0
 
-    const height = 28;
+    const height = 30;
 
-    let off_one_toggle=false;
+    let mode:string = "read"
+    let highlight = 0;
+
+    let off_one_toggle=false;  // display offset ROM (odd addresses) toggle
+
+    $: mode, handleTick()
 
     // whenever input rom changes, set page to 0 and generate a new 16bit version, store that to rom
     $: raw_rom, page=0, rom=generateu16(raw_rom)
 
+    // whenever we toggle offset ROM mode, reload rom with offset enabled or disabled
     $: off_one_toggle, rom = off_one_toggle?odd_generateu16(raw_rom):generateu16(raw_rom)
 
     // whenever page changes, update display to match page
-    $: page, display = rom.slice(page*height, (page*height)+height)
+    $: page, display = rom.slice(page*height, (page*height)+height), console.log("lalala")
+
 
     const generateu16 = (buff: Uint8Array) => {
         const padded_rom = new Uint8Array(buff.byteLength%2 == 0? buff.byteLength:buff.byteLength + 1);        
@@ -45,6 +55,7 @@
             return
         }
         page++;
+        mode="read"
     }
 
     const dinc_page = () => {
@@ -53,10 +64,24 @@
             return
         }
         page--;
+        mode = "read"
     }
 
     const swap_endian = (x: number) => {
         return ((x & 0x00ff) << 8 | (x>>8))
+    }
+
+    const handleTick = () => {
+        if (mode === "debug") {
+            const rel_add = Math.floor(pc-512)
+            if (rel_add % 2 === 0) {
+                
+            }
+            highlight = Math.floor(rel_add%height / 2)
+            page = Math.floor(rel_add/height)
+
+            console.log("lets look at ", highlight, page)
+        }
     }
 
 </script>
@@ -64,7 +89,9 @@
 loading rom...
 {:else}
 <div>
-    <button on:click={() => off_one_toggle = !off_one_toggle} >odd offset: {off_one_toggle?"ON":"OFF"}</button> viewing: {rom_name} {raw_rom.length} bytes
+    <button on:click={() => off_one_toggle = !off_one_toggle} >odd offset: {off_one_toggle?"ON":"OFF"}</button> 
+    <button on:click={() => mode = mode==="read"?"debug":"read"} >debug mode: {mode}</button>
+    viewing: {rom_name} {raw_rom.length} bytes
     <table cellspacing="0" cellpadding="0">
         <th>
             #
@@ -79,7 +106,7 @@ loading rom...
             ascii
         </th>
         {#each display as inst, line}
-        <tr>
+        <tr style={ mode==="read"?"":(line === highlight? "background-color: black; color: white":"")} >
             <td>
                 {((height*page) + 512 + (line*2) - (off_one_toggle?1:0) ).toString(16)}
             </td>
@@ -116,7 +143,7 @@ loading rom...
         read from rom: <input type="number" bind:value={arbit}> {chip8.read_mem(arbit).toString(16)}
     </div>
     <div>
-        <button on:click={chip8.tick} class="tick">tick cpu</button>
+        <button on:click={() => {pc = chip8.tick(); console.log("pc is at ", pc.toString(16)), handleTick()}} class="tick">tick cpu {pc.toString(16)}</button>
     </div>
 </div>
 {/if}
