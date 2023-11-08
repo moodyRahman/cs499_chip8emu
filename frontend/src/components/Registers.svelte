79af24729ded847210803b13f4e4bd509d4154ff
Moody Rahman <moody.byte@gmail.com>
2023-11-08 08:53:46 -0500

    display RAM adjacent to the PC

--- frontend/src/components/Registers.svelte
+++ frontend/src/components/Registers.svelte
@@ -5,20 +5,21 @@
 
     let registers: Uint16Array;
     $: registers_trigger, registers = chip8.read_all_registers();
+
+    let pc_highlight_index = 5;
 </script>
 
 <div>
-
     registers
     <div class="registers">
-    {#each registers.slice(0, 16) as register, i}
-        <span class="register">V{i.toString(16)} {register.toString(16)}  </span>
+        {#each registers.slice(0, 16) as register, i}
+            <span class="register">V{i.toString(16)} {register.toString(16)}  </span>
         {/each}
     </div>
     <div>
         {#each registers.slice(0, 16) as register, i}
             <span class="register">S{i.toString(16)} {register.toString(16)}  </span>
-            {/each}
+        {/each}
     </div>
     <div>
         <span class="register">pc: {registers[32]}</span>
@@ -27,21 +28,39 @@
         <span class="register">dt: {registers[35]}</span>
     </div>
     <div>
-        {#each chip8.ram_around_address(3, 3, registers[32]) as mem}
-            <span>
-                {mem.toString(16)}
+        {#each chip8.ram_around_address(pc_highlight_index, pc_highlight_index, registers[32]) as mem, i}
+            {#if i == pc_highlight_index}
+                <span class="highlight adjacent">
+                    {mem.toString(16).padStart(2, "0")}
+                </span>
+            {:else}
+            <span class="adjacent">
+                {mem.toString(16).padStart(2, "0")}
             </span>
+            {/if}
+
+            
             {/each}
         </div>
     </div>
 
 <style>
-        .registers {
+    .registers {
         width: 50%;
         display: grid;
         grid-template-columns: 1fr 1fr 1fr 1fr;
     }
     .register {
         margin-left: 10px;
+    }
+
+    .highlight {
+        background-color: black;
+        color: white;
+    }
+
+    .adjacent {
+        margin-left: 5px;
+        padding: 2px;
     }
 </style>
