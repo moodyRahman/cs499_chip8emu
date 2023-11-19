bc389953352fdc21569dc098397e53ab3f1d9ca4
Moody Rahman <moody.byte@gmail.com>
2023-11-19 18:38:39 -0500

    moved assembler to lib folder

--- frontend/src/lib/assembler/assembler.js
+++ frontend/src/lib/assembler/assembler.js
@@ -3,43 +3,43 @@ import { error } from "@sveltejs/kit";
 
 // Table to map instruction to their respective argument check/parse function
 const argVerifyTable = {
-    "CLS" : noArg,
-    "RET" : noArg,
-    "SYS" : addrArg,
-    "JP"  : jpArg,
+    "CLS": noArg,
+    "RET": noArg,
+    "SYS": addrArg,
+    "JP": jpArg,
     "CALL": addrArg,
-    "SE"  : seArg,
-    "SNE" : sneArg,
-    "LD"  : ldArg, //WIP!!!!
-    "ADD" : addArg,
-    "OR"  : regRegArg,
-    "AND" : regRegArg,
-    "XOR" : regRegArg,
-    "SUB" : regRegArg,
-    "SHR" : [regArg, regRegArg],
+    "SE": seArg,
+    "SNE": sneArg,
+    "LD": ldArg, //WIP!!!!
+    "ADD": addArg,
+    "OR": regRegArg,
+    "AND": regRegArg,
+    "XOR": regRegArg,
+    "SUB": regRegArg,
+    "SHR": [regArg, regRegArg],
     "SUBN": regRegArg,
-    "SHL" : [regArg, regRegArg],
-    "RND" : regByteArg,
-    "DRW" : regRegNibArg, 
-    "SKP" : regArg,
+    "SHL": [regArg, regRegArg],
+    "RND": regByteArg,
+    "DRW": regRegNibArg,
+    "SKP": regArg,
     "SKPN": regArg
 };
 
 // Below are the functions used to check the argument
 // validity and parse them if valid
 
-function noArg(words){
+function noArg(words) {
     if (words.length != 1) return 0;
     return -1;
 }
 
-function addrArg(words){
+function addrArg(words) {
     if (words.length != 2) return 1;
     if (+words[1] > 4095 || +words[1] < 0) return 4;
     return -1;
 }
 
-function regArg(words){
+function regArg(words) {
     if (words.length != 2) return 1;
     // Check if register A is in a valid format
     if (words[1][0] != "V") return 5;
@@ -51,12 +51,12 @@ function regArg(words){
     return -1;
 }
 
-function regByteArg(words){
+function regByteArg(words) {
     if (words.length != 3) return 2;
     // Check if register A is in a valid format
     if (words[1][0] != "V") return 5;
-    if (words[1][words[1].length-1] != ",") return 7;
-    const regA = +words[1].substring(1, words[1].length-1);
+    if (words[1][words[1].length - 1] != ",") return 7;
+    const regA = +words[1].substring(1, words[1].length - 1);
     if (isNaN(regA)) return 6;
     if (regA < 0 || regA > 15) return 6;
     // Check if data byte is in a valid format
@@ -69,12 +69,12 @@ function regByteArg(words){
     return -1;
 }
 
-function regRegArg(words){
+function regRegArg(words) {
     if (words.length != 3) return 2;
     // Check if register A is in a valid format
     if (words[1][0] != "V") return 5;
-    if (words[1][words[1].length-1] != ",") return 7;
-    const regA = +words[1].substring(1, words[1].length-1);
+    if (words[1][words[1].length - 1] != ",") return 7;
+    const regA = +words[1].substring(1, words[1].length - 1);
     if (isNaN(regA)) return 6;
     if (regA < 0 || regA > 15) return 6;
     // Check if register B is in a valid format
@@ -88,18 +88,18 @@ function regRegArg(words){
     return -1;
 }
 
-function regRegNibArg(words){
+function regRegNibArg(words) {
     if (words.length != 4) return 3;
     // Check if register A is in a valid format
     if (words[1][0] != "V") return 5;
-    if (words[1][words[1].length-1] != ",") return 7;
-    const regA = +words[1].substring(1, words[1].length-1);
+    if (words[1][words[1].length - 1] != ",") return 7;
+    const regA = +words[1].substring(1, words[1].length - 1);
     if (isNaN(regA)) return 6;
     if (regA < 0 || regA > 15) return 6;
     // Check if register B is in a valid format
     if (words[2][0] != "V") return 5;
-    if (words[2][words[2].length-1] != ",") return 7;
-    const regB = +words[2].substring(1, words[2].length-1);
+    if (words[2][words[2].length - 1] != ",") return 7;
+    const regB = +words[2].substring(1, words[2].length - 1);
     if (isNaN(regB)) return 6;
     if (regB < 0 || regB > 15) return 6;
     // Check if data nibble is in a valid format
@@ -114,7 +114,7 @@ function regRegNibArg(words){
 }
 
 // 0 stores INS, 1 stores addr, 3 stores jp type, 2 stores int/label
-function jpArg(words){
+function jpArg(words) {
     if (addrArg(words) == -1) {
         words[3] = 0;
         return -1
@@ -128,7 +128,7 @@ function jpArg(words){
     return 12;
 }
 
-function seArg(words){
+function seArg(words) {
     if (regByteArg(words) == -1) {
         words[1] = 0x30 | words[1];
         return -1;
@@ -141,7 +141,7 @@ function seArg(words){
     return 12;
 }
 
-function sneArg(words){
+function sneArg(words) {
     if (regByteArg(words) == -1) {
         words[1] = 0x40 | words[1];
         return -1;
@@ -154,7 +154,7 @@ function sneArg(words){
     return 12;
 }
 
-function ldArg(words){
+function ldArg(words) {
     if (regByteArg(words) == -1) {
         words[1] = 0x60 | words[1];
         return -1;
@@ -172,8 +172,8 @@ function ldArg(words){
         return -1;
     }
     if (words[1][0] == "V") {
-        if (words[1][words[1].length-1] != ",") return 7;
-        const regA = +words[1].substring(1, words[1].length-1);
+        if (words[1][words[1].length - 1] != ",") return 7;
+        const regA = +words[1].substring(1, words[1].length - 1);
         if (isNaN(regA)) return 6;
         if (regA < 0 || regA > 15) return 6;
         switch (words[2]) {
@@ -220,7 +220,7 @@ function ldArg(words){
     return 12;
 }
 
-function addArg(words){
+function addArg(words) {
     if (regByteArg(words) == -1) {
         words[1] = 0x70 | words[1];
         return -1;
@@ -258,8 +258,8 @@ ARGUMENT ERROR CODES (when format is invalid)
   12: Other arg error
 */
 
-function argumentErrorHandler(errorCode, lineNumber){
-    switch(errorCode) {
+function argumentErrorHandler(errorCode, lineNumber) {
+    switch (errorCode) {
         case (-1):
             throw new Error(`Critical Assembler Error, in line ${lineNumber}, valid arg triggered error handler!`);
         case (0):
@@ -299,11 +299,11 @@ function preProcessStrip(rawASM = "") {
     const lineNumberTable = [];
     // Remove whitespace, comments and blank lines, make table of line numbers
     let lineCount = 0;
-    for (const line of rawLines){
+    for (const line of rawLines) {
         lineCount++;
         const trimmedLine = line.trim();
-        if (trimmedLine.length != 0){
-            if (trimmedLine[0] != "#"){
+        if (trimmedLine.length != 0) {
+            if (trimmedLine[0] != "#") {
                 const commentSplit = trimmedLine.split("#");
                 cleanLines.push(commentSplit[0].trim());
                 lineNumberTable.push(lineCount);
@@ -313,7 +313,7 @@ function preProcessStrip(rawASM = "") {
     return [cleanLines, lineNumberTable];
 }
 
-export default function assemble(ASM = ""){
+export default function assemble(ASM = "") {
     // First strip out comments, blank lines and whitespace
     const strippedData = preProcessStrip(ASM);
     const cleanLines = strippedData[0];
@@ -325,9 +325,9 @@ export default function assemble(ASM = ""){
     let addr = 0;
     let lineCount = 0;
     const parserDebugTable = []
-    for (const line of cleanLines){
-        if (line[0] == "."){
-            if (line.substring(1,5) == "org "){
+    for (const line of cleanLines) {
+        if (line[0] == ".") {
+            if (line.substring(1, 5) == "org ") {
                 const orgArg = +line.substring(5)
                 if (!isNaN(orgArg)) {
                     if (orgArg >= addr) {
@@ -335,21 +335,21 @@ export default function assemble(ASM = ""){
                         parserDebugTable.push(`ORG: ${orgArg}`)
                     } else {
                         throw new Error(`Alignment Error, cannot align chunk without clobbering memory as defined in line ${lineNumberTable[lineCount]}!`);
-                    } 
+                    }
                 } else {
                     throw new Error(`Argument Error, unknown directive argument in line ${lineNumberTable[lineCount]}!`);
                 }
             } else {
                 throw new Error(`Syntax Error, unknown directive in line ${lineNumberTable[lineCount]}!`);
             }
-        } else if (line[line.length-1] == ":") {
-            if (addr+4 > 4095) {
-                throw new Error(`Memory Error, program exceedes memory limit at line ${lineNumberTable[lineCount]}!`);  
+        } else if (line[line.length - 1] == ":") {
+            if (addr + 4 > 4095) {
+                throw new Error(`Memory Error, program exceedes memory limit at line ${lineNumberTable[lineCount]}!`);
             }
             if (line.length == 1) {
                 throw new Error(`Label Error, no label name in line ${lineNumberTable[lineCount]}!`);
-            } 
-            const labelArg = line.substring(0, line.length-1)
+            }
+            const labelArg = line.substring(0, line.length - 1)
             if (!isNaN(+labelArg)) {
                 throw new Error(`Label Error, label name cannot be a number in line ${lineNumberTable[lineCount]}!`);
             }
@@ -357,20 +357,20 @@ export default function assemble(ASM = ""){
                 labelTable[labelArg] = addr;
                 parserDebugTable.push(`Label : ${addr}`)
             } else {
-                throw new Error(`Label Error, duplicate label name in line ${lineNumberTable[lineCount]}!`);   
+                throw new Error(`Label Error, duplicate label name in line ${lineNumberTable[lineCount]}!`);
             }
         } else {
-            if (addr+4 > 4095) {
-                throw new Error(`Memory Error, program exceedes memory limit at line ${lineNumberTable[lineCount]}!`);  
+            if (addr + 4 > 4095) {
+                throw new Error(`Memory Error, program exceedes memory limit at line ${lineNumberTable[lineCount]}!`);
             }
             const words = line.split(" ");
             const checkerFunction = argVerifyTable[words[0]];
             if (checkerFunction == undefined) {
                 throw new Error(`Syntax Error, unknown instruction in line ${lineNumberTable[lineCount]}!`);
-            } else if (typeof(checkerFunction) != "function") {
+            } else if (typeof (checkerFunction) != "function") {
                 let argFlag = false;
                 let argError;
-                for (const argType of checkerFunction){
+                for (const argType of checkerFunction) {
                     argError = argType(words)
                     if (argError == -1) {
                         argFlag = true;
@@ -385,20 +385,20 @@ export default function assemble(ASM = ""){
                 }
             }
             parserDebugTable.push(`${words[0]}, ${words[1]}, ${words[2]}, ${words[3]}`);
-            switch (words[0]){
+            switch (words[0]) {
                 case "CLS":
                     finalRom[addr] = 0x00;
-                    finalRom[addr+1] = 0xE0;
+                    finalRom[addr + 1] = 0xE0;
                     break;
                 case "RET":
                     finalRom[addr] = 0x00;
-                    finalRom[addr+1] = 0xEE;
+                    finalRom[addr + 1] = 0xEE;
                     break;
                 case "SYS":
                     let sysAddr = +words[1]
                     if (!isNaN(sysAddr)) {
                         finalRom[addr] = 0x00 | (sysAddr >> 8);
-                        finalRom[addr+1] = sysAddr & 0xFF;
+                        finalRom[addr + 1] = sysAddr & 0xFF;
                     } else {
                         labelRequestTable.push([addr, words[1], lineCount]);
                         finalRom[addr] = 0x00;
@@ -408,7 +408,7 @@ export default function assemble(ASM = ""){
                     if (words[3] == 0) {
                         if (!isNaN(+words[1])) {
                             finalRom[addr] = 0x10 | (+words[1] >> 8);
-                            finalRom[addr+1] = +words[1] & 0xFF;
+                            finalRom[addr + 1] = +words[1] & 0xFF;
                         } else {
                             labelRequestTable.push([addr, words[1], lineCount]);
                             finalRom[addr] = 0x10;
@@ -416,7 +416,7 @@ export default function assemble(ASM = ""){
                     } else {
                         if (!isNaN(+words[2])) {
                             finalRom[addr] = 0xB0 | (+words[2] >> 8);
-                            finalRom[addr+1] = +words[2] & 0xFF;
+                            finalRom[addr + 1] = +words[2] & 0xFF;
                         } else {
                             labelRequestTable.push([addr, words[2], lineCount]);
                             finalRom[addr] = 0xB0;
@@ -427,7 +427,7 @@ export default function assemble(ASM = ""){
                     let callAddr = +words[1]
                     if (!isNaN(callAddr)) {
                         finalRom[addr] = 0x20 | (callAddr >> 8);
-                        finalRom[addr+1] = callAddr & 0xFF;
+                        finalRom[addr + 1] = callAddr & 0xFF;
                     } else {
                         labelRequestTable.push([addr, words[1], lineCount]);
                         finalRom[addr] = 0x20;
@@ -435,76 +435,76 @@ export default function assemble(ASM = ""){
                     break;
                 case "SE":
                     finalRom[addr] = words[1];
-                    finalRom[addr+1] = words[2];
+                    finalRom[addr + 1] = words[2];
                     break;
                 case "SNE":
                     finalRom[addr] = words[1];
-                    finalRom[addr+1] = words[2];
+                    finalRom[addr + 1] = words[2];
                     break;
                 case "LD":
                     if (words[3] == 1) {
                         let ldAddr = +words[2]
                         if (!isNaN(ldAddr)) {
                             finalRom[addr] = 0xA0 | (ldAddr >> 8);
-                            finalRom[addr+1] = ldAddr & 0xFF;
+                            finalRom[addr + 1] = ldAddr & 0xFF;
                         } else {
                             labelRequestTable.push([addr, words[2], lineCount]);
                             finalRom[addr] = 0xA0;
                         }
                     } else {
                         finalRom[addr] = words[1];
-                        finalRom[addr+1] = words[2];
+                        finalRom[addr + 1] = words[2];
                     }
                     break;
                 case "ADD":
                     finalRom[addr] = words[1];
-                    finalRom[addr+1] = words[2];
+                    finalRom[addr + 1] = words[2];
                     break;
                 case "OR":
                     finalRom[addr] = 0x80 | words[1];
-                    finalRom[addr+1] = 0x01 | (words[2] << 4);
+                    finalRom[addr + 1] = 0x01 | (words[2] << 4);
                     break;
                 case "AND":
                     finalRom[addr] = 0x80 | words[1];
-                    finalRom[addr+1] = 0x02 | (words[2] << 4);
+                    finalRom[addr + 1] = 0x02 | (words[2] << 4);
                     break;
                 case "XOR":
                     finalRom[addr] = 0x80 | words[1];
-                    finalRom[addr+1] = 0x03 | (words[2] << 4);
+                    finalRom[addr + 1] = 0x03 | (words[2] << 4);
                     break;
                 case "SUB":
                     finalRom[addr] = 0x80 | words[1];
-                    finalRom[addr+1] = 0x05 | (words[2] << 4);
+                    finalRom[addr + 1] = 0x05 | (words[2] << 4);
                     break;
                 case "SHR":
                     finalRom[addr] = 0x80 | words[1];
                     if (words[2] == undefined) words[2] == 0;
-                    finalRom[addr+1] = 0x06 | (words[2] << 4);
+                    finalRom[addr + 1] = 0x06 | (words[2] << 4);
                     break;
                 case "SUBN":
                     finalRom[addr] = 0x80 | words[1];
-                    finalRom[addr+1] = 0x07 | (words[2] << 4);
+                    finalRom[addr + 1] = 0x07 | (words[2] << 4);
                     break;
                 case "SHL":
                     finalRom[addr] = 0x80 | words[1];
                     if (words[2] == undefined) words[2] == 0;
-                    finalRom[addr+1] = 0x0E | (words[2] << 4);
+                    finalRom[addr + 1] = 0x0E | (words[2] << 4);
                     break;
                 case "RND":
                     finalRom[addr] = 0xC0 | words[1];
-                    finalRom[addr+1] = words[2];
+                    finalRom[addr + 1] = words[2];
                     break;
                 case "DRW":
                     finalRom[addr] = 0xD0 | words[1];
-                    finalRom[addr+1] = (words[2] << 4) | words[3];
+                    finalRom[addr + 1] = (words[2] << 4) | words[3];
                     break;
                 case "SKP":
                     finalRom[addr] = 0xE0 | words[1];
-                    finalRom[addr+1] = 0x9E;
+                    finalRom[addr + 1] = 0x9E;
                     break;
                 case "SKPN":
                     finalRom[addr] = 0xE0 | words[1];
-                    finalRom[addr+1] = 0xA1;
+                    finalRom[addr + 1] = 0xA1;
                     break;
                 default:
                     throw new Error(`Critical Assembler Error, in line ${lineNumberTable[lineCount]}, invalid instruction made it past the checker!`);
@@ -518,7 +518,7 @@ export default function assemble(ASM = ""){
             throw new Error(`Label Error, no such label as ${request[1]} on line ${lineNumberTable[request[2]]}`)
         }
         finalRom[request[0]] |= (labelTable[request[1]] >> 8);
-        finalRom[request[0]+1] = 0xFF & labelTable[request[1]];
+        finalRom[request[0] + 1] = 0xFF & labelTable[request[1]];
     }
     console.log(ASM.split("\n"))
     console.log(cleanLines)
