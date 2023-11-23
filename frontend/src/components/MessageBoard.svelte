eb2744ead8c8f37162eeeb318b16ff74fe41505d
Moody Rahman <moody.byte@gmail.com>
2023-11-23 14:41:07 -0500

    display rom metadata

---
+++ frontend/src/components/MessageBoard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { rom_description, rom_mappings, rom_status } from "$lib/stores/cpu_state";
+
+
+</script>
+
+<div>
+    <div>
+        {$rom_description}
+    </div>
+    <div>
+        {#each $rom_mappings as mappings}
+        <div>
+            {mappings?.keyboard}: {mappings?.description}
+        </div>
+        {/each}
+    </div>
+</div>
+
+
+<style>
+
+</style>
