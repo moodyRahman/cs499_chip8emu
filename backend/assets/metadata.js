54e57b96e8c6c2a284ece724102651641984b879
Moody Rahman <moody.byte@gmail.com>
2023-11-30 21:59:28 -0500

    rom metadata

--- backend/assets/metadata.js
+++ backend/assets/metadata.js
@@ -80,10 +80,125 @@ const data = {
     "mapping": [],
     "timing": {
       "ticks_per_interval": 1,
-      "time_between_intervals_ms": 1000,
+      "time_between_intervals_ms": 20,
       "display_rerender_threshold": 1
     }
-  }
+  },
+  "SpaceInvaders.ch8": {
+    "description": "Shoot em dead!",
+    "mapping": [
+      {
+        "keyboard": "q",
+        "description": "move your ship left"
+      },
+      {
+        "keyboard": "w",
+        "description": "shoot"
+      },
+      {
+        "keyboard": "e",
+        "description": "move your ship right"
+      },
+    ],
+    "timing": {
+      "ticks_per_interval": 13,
+      "time_between_intervals_ms": 8,
+      "display_rerender_threshold": 13
+    }
+  },
+  "CONNECT4.ch8": {
+    "description": "A classic game",
+    "mapping": [
+      {
+        "keyboard": "q",
+        "description": "move your selector left"
+      },
+      {
+        "keyboard": "w",
+        "description": "place piece"
+      },
+      {
+        "keyboard": "e",
+        "description": "move your selector right"
+      },
+    ],
+    "timing": {
+      "ticks_per_interval": 3,
+      "time_between_intervals_ms": 8,
+      "display_rerender_threshold": 1
+    }
+  },
+
+  "BRIX.ch8": {
+    "description": "Breakout clone",
+    "mapping": [
+      {
+        "keyboard": "q",
+        "description": "move your paddle left"
+      },
+      {
+        "keyboard": "w",
+        "description": "launch ball"
+      },
+      {
+        "keyboard": "e",
+        "description": "move your paddle right"
+      },
+    ],
+    "timing": {
+      "ticks_per_interval": 6,
+      "time_between_intervals_ms": 8,
+      "display_rerender_threshold": 6
+    }
+  },
+  "Breakout.ch8": {
+    "description": "Breakout clone",
+    "mapping": [
+      {
+        "keyboard": "q",
+        "description": "move your paddle left"
+      },
+      {
+        "keyboard": "w",
+        "description": "launch ball"
+      },
+      {
+        "keyboard": "e",
+        "description": "move your paddle right"
+      },
+    ],
+    "timing": {
+      "ticks_per_interval": 8,
+      "time_between_intervals_ms": 10,
+      "display_rerender_threshold": 8
+    }
+  },
+  "Pong2.ch8": {
+    "description": "2 player pong",
+    "mapping": [
+      {
+        "keyboard": "1",
+        "description": "move left paddle up"
+      },
+      {
+        "keyboard": "q",
+        "description": "move left paddle down"
+      },
+      {
+        "keyboard": "4",
+        "description": "move right paddle up"
+      },
+      {
+        "keyboard": "r",
+        "description": "move right paddle down"
+      },
+    ],
+    "timing": {
+      "ticks_per_interval": 8,
+      "time_between_intervals_ms": 8,
+      "display_rerender_threshold": 8
+    }
+  },
 }
 
 
