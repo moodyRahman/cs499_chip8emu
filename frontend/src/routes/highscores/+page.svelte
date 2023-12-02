d37c3f89f16616564d94ddc410e65dc6fe829343
Moody Rahman <moody.byte@gmail.com>
2023-12-02 13:51:42 -0500

    implemented highscores page

--- frontend/src/routes/highscores/+page.svelte
+++ frontend/src/routes/highscores/+page.svelte
@@ -1,6 +1,55 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+	import config from "../../cpu_configs";
+	import { implemented_games } from "$lib/highscore";
 
+    let data:{
+        game: string,
+        name: string,
+        score: number
+    }[] = []
+
+
+
+    onMount(async  () => {
+        const res = await fetch(`${config.backend_url}/highscores`)
+        data = await res.json()
+    })
 </script>
 
 
-highscore board!
+<div class="container">
+    {#each implemented_games as game}
+    <div>
+
+        {game}:
+        <table>
+            <tr>
+                <th>name</th>
+                <th>score</th>
+            </tr>
+            
+            {#each data.filter((x) => x.game === game) as score}
+            <tr>
+                <td>
+                    {score.name}
+                    
+                </td>
+                <td>
+                    {score.score}
+                </td>
+            </tr>
+            {/each}
+        </table>
+    </div>    
+    {/each}
+</div>
+
+<style>
+
+    .container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+</style>
