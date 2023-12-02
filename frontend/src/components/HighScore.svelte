6b13f307fd20a71ccbc5f5269e40f3bc6b285099
Moody Rahman <moody.byte@gmail.com>
2023-12-02 11:34:01 -0500

    error modal + handle score uploads

--- frontend/src/components/HighScore.svelte
+++ frontend/src/components/HighScore.svelte
@@ -1,30 +1,89 @@
 <script lang="ts">
-	import highScore from "$lib/highscore";
+	import highScore, { highScoreInfo } from "$lib/highscore";
+	import { rom_name as rom_name_store } from "$lib/stores/cpu_state";
 	import { onMount } from "svelte";
+	import config from "../cpu_configs";
+	import BadWordsFilter from "bad-words"; // genuinely awful library, feel free to replace
 
-    let dialog: any;
+    let submit_dialog: any;
+    let error_dialog: any;
     let name: string = ""
-    onMount(() => {
-        dialog = document.querySelector("dialog");
+    
+    let score = 0
 
+    const filter = new BadWordsFilter()
+    const implemented_games = Object.values(highScoreInfo).map((x) => x[0])
+
+    $: rom_name = $rom_name_store.substring(0, $rom_name_store.length - 4)
+    onMount(() => {
+        submit_dialog = document.querySelector("#submit-dialog");
+        error_dialog = document.querySelector("#error-dialog");
     })
 
+
     const openModal = (e: MouseEvent) => {
-        dialog?.showModal();    
-        console.log(highScore())
+        if (!implemented_games.includes(rom_name))
+        {
+            error_dialog?.showModal();
+        }
+        else {
+            score = highScore()!
+            submit_dialog?.showModal();    
+        }
+    }
+
+    const handleScoreSubmit = async () => {
+
+        if (filter.isProfane(name)) {
+            name = ""
+            return;
+        }
+
+        const res = await fetch(`${config.backend_url}/highscores`, {
+            method:"POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(
+                {
+                    name: name,
+                    score: score,
+                    game: rom_name
+                })
+        });
+
+        const data = await res.json()
+        console.log(data)
+
     }
 
 </script>
 
-<dialog>
+<dialog id="submit-dialog">
+    <div>
+        score: {score}
+    </div>
     <div>
         enter your name: <input type="text" bind:value={name}>
     </div>
     <div>
-        <button on:click={() => dialog?.close()}>cancel</button>
-        <button on:click={() => dialog?.close()}>submit</button>
+        <button on:click={() => submit_dialog?.close()}>cancel</button>
+        <button on:click={handleScoreSubmit}>submit</button>
 
     </div>
+</dialog>
+
+<dialog id="error-dialog">
+    <div>
+        sorry! no highscores implemented for this game, try one of the following games instead:
+        {#each implemented_games as game}
+         <div>
+             - {game}
+         </div>
+        {/each}
+    </div>
+    <button on:click={() => error_dialog?.close()}>cancel</button>
 </dialog>
 
 <button on:click={openModal}>
